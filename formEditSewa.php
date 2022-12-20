42af58fc3f107874ff0ce52eb7861cf713329c84
Achmat Fajri <achmatfajri@gmail.com>
2022-12-19 19:26:01 -0800

    fixing style

--- formEditSewa.php
+++ formEditSewa.php
@@ -17,21 +17,30 @@ $row = mysqli_fetch_array($query);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Edit Sewa</title>
+    <link rel="stylesheet" href="table.css">
 </head>
 
 <body>
-    <!-- Disini saya membuat form untuk mengupdate data yang merupakan fungsi update pada CRUD dan menambahkan valuenya -->
-    <form action="prosesEditSewa.php?id=<?php echo $id; ?>" method="post">
-        <label>Judul Buku</label>
-        <div><input type="text" name="judul" value="<?php echo $row['judul']; ?>"></div>
-        <label>Penyewa</label>
-        <div><input type="text" name="penyewa" value="<?php echo $row['penyewa']; ?>"></div>
-        <label>Durasi Sewa</label>
-        <div><input type="text" name="durasi" value="<?php echo $row['durasi']; ?>"></div>
-        <div>
-            <input type="submit" name="sewa" value="Update data">
+    <nav>
+        <div class="container">
+            <h1>CRUD PHP MYSQL</h1>
         </div>
-    </form>
+    </nav>
+    <div class="container">
+        <h1>Edit data</h1>
+        <!-- Disini saya membuat form untuk mengupdate data yang merupakan fungsi update pada CRUD dan menambahkan valuenya -->
+        <form action="prosesEditSewa.php?id=<?php echo $id; ?>" method="post">
+            <label>Judul Buku</label>
+            <div><input type="text" name="judul" value="<?php echo $row['judul']; ?>"></div>
+            <label>Penyewa</label>
+            <div><input type="text" name="penyewa" value="<?php echo $row['penyewa']; ?>"></div>
+            <label>Durasi Sewa</label>
+            <div><input type="text" name="durasi" value="<?php echo $row['durasi']; ?>"></div>
+            <div>
+                <input type="submit" name="sewa" value="Update data">
+            </div>
+        </form>
+    </div>
 
 </body>
 
--- formSewa.php
+++ formSewa.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Sewa Buku</title>
+    <link rel="stylesheet" href="table.css">
 </head>
+
 <body>
-<!-- Disini saya membuat form untuk memasukan data yang merupakan fungsi create pada CRUD, lalu datanya akan diproses pada file prosesSewa.php -->
-    <form action="prosesSewa.php" method="post">
-        <label>Judul Buku</label>
-        <div><input type="text" name="judul"></div>
-        <label>Penyewa</label>
-        <div><input type="text" name="penyewa"></div>
-        <label>Durasi Sewa</label>
-        <div><input type="text" name="durasi"></div>
-        <div>
-            <input type="submit" name="sewa" value="Sewa Buku">
+    <nav>
+        <div class="container">
+            <h1>CRUD PHP MYSQL</h1>
         </div>
-    </form>
+    </nav>
+    <div class="container">
+        <h1>Tambah Penyewaan Buku</h1>
+        <!-- Disini saya membuat form untuk memasukan data yang merupakan fungsi create pada CRUD, lalu datanya akan diproses pada file prosesSewa.php -->
+        <form action="prosesSewa.php" method="post">
+            <label>Judul Buku</label>
+            <div><input type="text" name="judul"></div>
+            <label>Penyewa</label>
+            <div><input type="text" name="penyewa"></div>
+            <label>Durasi Sewa</label>
+            <div><input type="text" name="durasi"></div>
+            <div>
+                <input type="submit" name="sewa" value="Sewa Buku">
+            </div>
+        </form>
+    </div>
 
     <!-- saya tidak membuat input untuk tanggal sewanya karena ini bisa saja terjadi kecurangan oleh admin, jadi nanti saya akan atur tanggalnya otomatis -->
 </body>
+
 </html>
